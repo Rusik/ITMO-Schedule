99928e7624038c977159e6b03a48d094a57e9f27
Ruslan Kavetsky <rusya.182@gmail.com>
2013-12-05 21:04:23 +0400

    Add splash screen. Blur depends on device. Add missing icons.

--- ITMOSchedule/Sources/Views/OUTopView.m
+++ ITMOSchedule/Sources/Views/OUTopView.m
@@ -13,6 +13,10 @@
 #import "OUScheduleDownloader.h"
 #import "UIView+Helpers.h"
 
+#import <sys/types.h>
+#import <sys/sysctl.h>
+
+
 @interface OUTopView () <UITextFieldDelegate>
 
 @end
@@ -47,7 +51,7 @@
     _blurView.viewsToHide = @[self];
 
 //    turn off for debug
-//    _blurView.dynamic = NO;
+    _blurView.dynamic = [self supportBlur];
 
     [self updateInfoLabel];
 
@@ -238,6 +242,88 @@
     if (active) {
         _textField.text = nil;
     }
+}
+
+- (BOOL)supportBlur {
+    size_t size;
+    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+    char *model = malloc(size);
+    sysctlbyname("hw.machine", model, &size, NULL, 0);
+    NSString *platform = [NSString stringWithCString:model encoding:NSUTF8StringEncoding];
+    free(model);
+
+    //    if ([platform isEqualToString:@"iPhone1,1"])    return @"iPhone 1G";
+    //    if ([platform isEqualToString:@"iPhone1,2"])    return @"iPhone 3G";
+    //    if ([platform isEqualToString:@"iPhone2,1"])    return @"iPhone 3GS";
+    //    if ([platform isEqualToString:@"iPhone3,1"])    return @"iPhone 4";
+    //    if ([platform isEqualToString:@"iPhone3,3"])    return @"Verizon iPhone 4";
+    //    if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
+    //    if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5 (GSM)";
+    //    if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5 (GSM+CDMA)";
+    //    if ([platform isEqualToString:@"iPhone5,3"])    return @"iPhone 5c (GSM)";
+    //    if ([platform isEqualToString:@"iPhone5,4"])    return @"iPhone 5c (Global)";
+    //    if ([platform isEqualToString:@"iPhone6,1"])    return @"iPhone 5s (GSM)";
+    //    if ([platform isEqualToString:@"iPhone6,2"])    return @"iPhone 5s (Global)";
+    //
+    //    if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
+    //    if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
+    //    if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
+    //    if ([platform isEqualToString:@"iPod4,1"])      return @"iPod Touch 4G";
+    //    if ([platform isEqualToString:@"iPod5,1"])      return @"iPod Touch 5G";
+    //
+    //    if ([platform isEqualToString:@"iPad1,1"])      return @"iPad";
+    //    if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2 (WiFi)";
+    //    if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2 (GSM)";
+    //    if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2 (CDMA)";
+    //    if ([platform isEqualToString:@"iPad2,4"])      return @"iPad 2 (WiFi)";
+    //    if ([platform isEqualToString:@"iPad2,5"])      return @"iPad Mini (WiFi)";
+    //    if ([platform isEqualToString:@"iPad2,6"])      return @"iPad Mini (GSM)";
+    //    if ([platform isEqualToString:@"iPad2,7"])      return @"iPad Mini (GSM+CDMA)";
+    //    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad 3 (WiFi)";
+    //    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad 3 (GSM+CDMA)";
+    //    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad 3 (GSM)";
+    //    if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4 (WiFi)";
+    //    if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4 (GSM)";
+    //    if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4 (GSM+CDMA)";
+    //    if ([platform isEqualToString:@"iPad4,1"])      return @"iPad Air (WiFi)";
+    //    if ([platform isEqualToString:@"iPad4,2"])      return @"iPad Air (GSM)";
+    //    if ([platform isEqualToString:@"iPad4,4"])      return @"iPad Mini Retina (WiFi)";
+    //    if ([platform isEqualToString:@"iPad4,5"])      return @"iPad Mini Retina (GSM)";
+    //
+    //    if ([platform isEqualToString:@"i386"])         return @"Simulator";
+    //    if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
+
+    NSArray *components = [platform componentsSeparatedByString:@","];
+    NSString *aux;
+    NSString *aux2;
+    if (components.count == 2) {
+        aux = components[0];
+        aux2 = components[1];
+    } else {
+        return NO;
+    }
+
+    // начиная с пятого
+    if ([aux rangeOfString:@"iPhone"].location != NSNotFound) {
+        int version = [[aux stringByReplacingOccurrencesOfString:@"iPhone" withString:@""] intValue];
+        return version > 4;
+    }
+
+    // начиная с длинного (пятого)
+    if ([aux rangeOfString:@"iPod"].location != NSNotFound) {
+        int version = [[aux stringByReplacingOccurrencesOfString:@"iPod" withString:@""] intValue];
+        return version > 4;
+    }
+
+    // начиная с четвёртого
+    if ([aux rangeOfString:@"iPad"].location != NSNotFound) {
+        int version = [[aux stringByReplacingOccurrencesOfString:@"iPad" withString:@""] intValue];
+        int number = aux2.intValue;
+        if (version == 3 && number > 3) return YES;
+        if (version > 3) return YES;
+    }
+
+    return NO;
 }
 
 @end
